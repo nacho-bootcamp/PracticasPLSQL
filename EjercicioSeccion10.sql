c360f2fc5ba13e187b40886008c418952630eb39
ignacio leonel cardozo <ignaciocardozoxd12@gmail.com>
2023-10-21 23:54:29 -0300

    ejercicio 10 segundo punto

--- EjercicioSeccion10.sql
+++ EjercicioSeccion10.sql
@@ -29,3 +29,59 @@ BEGIN
         END IF;
     END LOOP;
 END;
+/
+
+/*• 2- Vamos averiguar cuales son los JEFES (MANAGER_ID) de cada 
+departamento. En la tabla DEPARTMENTS figura el MANAGER_ID de cada 
+departamento, que a su vez es también un empleado. Hacemos un bloque con 
+dos cursores. (Esto se puede hacer fácilmente con una sola SELECT pero vamos 
+a hacerlo de esta manera para probar parámetros en cursores). 
+o El primero de todos los empleados 
+o El segundo de departamentos, buscando el MANAGER_ID con el 
+parámetro que se le pasa. 
+o Por cada fila del primero, abrimos el segundo cursor pasando el 
+EMPLOYEE_ID 
+o Si el empleado  es MANAGER_ID en algún departamento debemos 
+pintar el Nombre del departamento y el nombre del MANAGER_ID 
+diciendo que es el jefe. 
+o Si el empleado no es MANAGER de ningún departamento debemos 
+poner “No es jefe de nada” */
+
+SET SERVEROUTPUT ON
+
+DECLARE
+    departamento departments%rowtype;
+    jefe         departments.manager_id%TYPE;
+    CURSOR c1 IS
+    SELECT
+        *
+    FROM
+        employees;
+
+    CURSOR c2 (
+        j departments.manager_id%TYPE
+    ) IS
+    SELECT
+        *
+    FROM
+        departments
+    WHERE
+        manager_id = j;
+
+BEGIN
+    FOR empleado IN c1 LOOP
+        OPEN c2(empleado.employee_id);
+        FETCH c2 INTO departamento;
+        IF c2%notfound THEN
+            dbms_output.put_line(empleado.first_name || ' No es JEFE de 
+NADA');
+        ELSE
+            dbms_output.put_line(empleado.first_name
+                                 || 'ES JEFE DEL 
+DEPARTAMENTO '
+                                 || departamento.department_name);
+        END IF;
+
+        CLOSE c2;
+    END LOOP;
+END;
