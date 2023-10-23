5c470a17fa11d19cb92492dd0a30d3b459d61f2e
ignacio leonel cardozo <ignaciocardozoxd12@gmail.com>
2023-10-22 23:49:50 -0300

    ejercicio 3

--- EjercicioSeccion10.sql
+++ EjercicioSeccion10.sql
@@ -18,24 +18,18 @@ BEGIN
             i.first_name = 'Steven'
             AND i.last_name = 'King'
         THEN
-            raise_application_error(-20300, 'El salario del jefe no puede ser visto');
+            raise_application_error(-20001, 'EL SALARIO DEL JEFE NO SE DEBE MOSTRAR ');
         ELSE
-            dbms_output.put_line(i.first_name
-                                 || ' '
-                                 || i.last_name
-                                 || ': '
-                                 || i.salary
-                                 || 'DLS');
+            dbms_output.put_line(i.salary);
         END IF;
     END LOOP;
 END;
-/
-
+/   
 /*• 2- Vamos averiguar cuales son los JEFES (MANAGER_ID) de cada 
 departamento. En la tabla DEPARTMENTS figura el MANAGER_ID de cada 
 departamento, que a su vez es también un empleado. Hacemos un bloque con 
 dos cursores. (Esto se puede hacer fácilmente con una sola SELECT pero vamos 
-a hacerlo de esta manera para probar parámetros en cursores). 
+a hacerlo de esta manera para probar parámetros en cursores).
 o El primero de todos los empleados 
 o El segundo de departamentos, buscando el MANAGER_ID con el 
 parámetro que se le pasa. 
@@ -46,7 +40,6 @@ pintar el Nombre del departamento y el nombre del MANAGER_ID
 diciendo que es el jefe. 
 o Si el empleado no es MANAGER de ningún departamento debemos 
 poner “No es jefe de nada” */
-
 SET SERVEROUTPUT ON
 
 DECLARE
@@ -84,4 +77,22 @@ DEPARTAMENTO '
 
         CLOSE c2;
     END LOOP;
+END;
+/
+
+/*• 3-Crear un cursor con parámetros que pasando el número de departamento 
+visualice el número de empleados de ese departamento */
+
+SET SERVEROUTPUT ON
+DECLARE
+CODIGO DEPARTMENTS.DEPARTMENT_ID%TYPE;
+CURSOR C1(NUMDEP DEPARTMENTS.DEPARTMENT_ID%TYPE ) IS SELECT COUNT(*) FROM EMPLOYEES
+WHERE department_id=NUMDEP;
+NUM_EMPLE NUMBER;
+BEGIN
+CODIGO:=10;
+OPEN C1(CODIGO);
+FETCH C1 INTO NUM_EMPLE;
+DBMS_OUTPUT.PUT_LINE('numero de empleados de ' ||codigo||' es 
+'||num_emple);
 END;
