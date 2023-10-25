6a47927a539ca33b91368631f13d10c64d7783e4
ignacio leonel cardozo <ignaciocardozoxd12@gmail.com>
2023-10-25 20:35:45 -0300

    PARAMETRO DE TIPO OUT

--- ProbandoProcedimientos.sql
+++ ProbandoProcedimientos.sql
@@ -12,3 +12,18 @@ begin
 calc_tax(10,70);
 end;
 /
+
+--------------------------PARAMETRO DE TIPO OUT ---------------------------------
+set serveroutput on
+DECLARE
+A NUMBER;
+B NUMBER;
+R NUMBER;
+begin
+A:=120;
+B:=10;
+R:=0;
+calc_tax_OUT(A,B,R);
+DBMS_OUTPUT.PUT_LINE('R= '||R);
+end;
+/
