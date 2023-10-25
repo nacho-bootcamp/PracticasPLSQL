9d625b44d9d9c46ba8afcbf9f72e565c25747a65
ignacio leonel cardozo <ignaciocardozoxd12@gmail.com>
2023-10-25 20:21:31 -0300

    PARAMETRO DE TIPO IN

--- ProbandoProcedimientos.sql
+++ ProbandoProcedimientos.sql
@@ -1,7 +1,14 @@
-SET SERVEROUTPUT ON
+ SET SERVEROUTPUT ON
 BEGIN
 PR1;
 END;
 /
 
 EXECUTE pr1;
+
+--------------------------PARAMETRO DE TIPO IN ---------------------------------
+set serveroutput on
+begin
+calc_tax(10,70);
+end;
+/
