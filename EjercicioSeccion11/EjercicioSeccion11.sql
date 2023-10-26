5c7588c2499d66ef7fabc675d01d720e574a8ba3
ignacio leonel cardozo <ignaciocardozoxd12@gmail.com>
2023-10-25 21:19:45 -0300

    ejercicios

--- EjercicioSeccion11/EjercicioSeccion11.sql
+++ EjercicioSeccion11/EjercicioSeccion11.sql
@@ -3,13 +3,53 @@
 salario de todos los empleados.
 */
 
-CREATE OR REPLACE PROCEDURE VISUALIZAR IS
+CREATE OR REPLACE PROCEDURE visualizar IS
 
-CURSOR C1 IS SELECT FIRST_NAME,SALARY FROM EMPLOYEES;
-NOMBRE EMPLOYEES.FIRST_NAME%TYPE;
-SALARIO EMPLOYEES.SALARY%TYPE;
+    CURSOR c1 IS
+    SELECT
+        first_name,
+        salary
+    FROM
+        employees;
+
+    nombre  employees.first_name%TYPE;
+    salario employees.salary%TYPE;
 BEGIN
-FOR I IN C1 LOOP
-DBMS_OUTPUT.PUT_LINE(I.FIRST_NAME||'  '||I.SALARY);
-END LOOP;
+    FOR i IN c1 LOOP
+        dbms_output.put_line(i.first_name
+                             || '  '
+                             || i.salary);
+    END LOOP;
+END;
+/
+/*2- Modificar el programa anterior para incluir un parámetro que pase el
+número de departamento para que visualice solo los empleados de ese
+departamento
+• Debe devolver el número de empleados en una variable de tipo OUT*/
+
+CREATE OR REPLACE PROCEDURE visualizar (
+    departamento NUMBER,
+    numero       OUT NUMBER
+) IS
+
+    CURSOR c1 IS
+    SELECT
+        first_name,
+        salary
+    FROM
+        employees
+    WHERE
+        department_id = departamento;
+
+    nombre  employees.first_name%TYPE;
+    salario employees.salary%TYPE;
+BEGIN
+    numero := 0;
+    FOR i IN c1 LOOP
+        dbms_output.put_line(i.first_name
+                             || ' '
+                             || i.salary);
+        numero := numero + 1;
+    END LOOP;
+
 END;
--- EjercicioSeccion11/ProbandoEjercicios11.sql
+++ EjercicioSeccion11/ProbandoEjercicios11.sql
@@ -1,7 +1,23 @@
 /*1- Crear un procedimiento llamado “visualizar” que visualice el nombre y
 salario de todos los empleados.
-*/ 
+*/
 SET SERVEROUTPUT ON
+
 BEGIN
-visualizar;
+    visualizar;
+END;
+/
+/*2- Modificar el programa anterior para incluir un parámetro que pase el
+número de departamento para que visualice solo los empleados de ese
+departamento
+• Debe devolver el número de empleados en una variable de tipo OUT*/
+
+set SERVEROUTPUT ON
+
+DECLARE
+    x NUMBER;
+BEGIN
+    x := 10;
+    visualizar(70, x);
+    dbms_output.put_line('El número de empleados es:' || x);
 END;
