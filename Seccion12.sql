dd2d265c07bebc1ab7e6f35a606d3df26bd572b4
ignacio leonel cardozo <ignaciocardozoxd12@gmail.com>
2023-10-31 00:30:12 -0300

    CREAR EL CUERPO DE UN PAQUETE

--- Seccion12.sql
+++ Seccion12.sql
@@ -22,4 +22,46 @@ CREATE OR REPLACE PACKAGE pack1 IS
     v2 VARCHAR2(100);
 END;
 /
-/**puedo crear variables globales en toda la app que perciba durante la sesion/
+/*puedo crear variables globales en toda la app que perciba durante la sesion*/
+
+------------------------CREAR EL CUERPO DE UN PAQUETE---------------------------
+CREATE OR REPLACE PACKAGE pack1 IS
+    PROCEDURE convert (
+        name            VARCHAR2,
+        conversion_type CHAR
+    );
+
+END;
+/
+
+CREATE OR REPLACE PACKAGE BODY pack1 IS
+
+    FUNCTION up (
+        name VARCHAR2
+    ) RETURN VARCHAR2 IS
+    BEGIN
+        RETURN upper(name);
+    END up;
+
+    FUNCTION do (
+        name VARCHAR2
+    ) RETURN VARCHAR2 IS
+    BEGIN
+        RETURN lower(name);
+    END do;
+
+    PROCEDURE convert (
+        name            VARCHAR2,
+        conversion_type CHAR
+    ) IS
+    BEGIN
+        IF conversion_type = 'U' THEN
+            dbms_output.put_line(up(name));
+        ELSIF conversion_type = 'L' THEN
+            dbms_output.put_line(do(name));
+        ELSE
+            dbms_output.put_line('EL PARAMETRO DEBE SER U O L');
+        END IF;
+    END convert;
+
+END pack1;
