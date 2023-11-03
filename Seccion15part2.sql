5005e8eb7701bb6b5da24121baa415f9b8f6480a
ignacio leonel cardozo <ignaciocardozoxd12@gmail.com>
2023-11-02 21:14:37 -0300

    USAR FUNCIONES JSON EN PL/SQL

--- Seccion15part2.sql
+++ Seccion15part2.sql
@@ -16,3 +16,65 @@ dbms_output.put_line(c1);
 
 end;
 /
+
+---------------------------USAR FUNCIONES JSON EN PL/SQL------------------------
+
+SET SERVEROUTPUT ON FORMAT WRAPPED LINE 1000;
+ 
+DECLARE
+C1 VARCHAR2(400);
+BEGIN
+SELECT
+    prod1.datos.poblacion
+INTO c1
+FROM
+    productos1 prod1
+WHERE
+    codigo = 2;
+
+dbms_output.put_line(c1);
+
+SELECT JSON_VALUE(PROD1.DATOS,'$.pais') INTO C1 FROM PRODUCTOS1 PROD1 WHERE CODIGO=2;
+dbms_output.put_line(c1);
+
+--no soprta info de tipo escalar
+SELECT JSON_VALUE(PROD1.DATOS,'$.direccion.calle') INTO C1 FROM PRODUCTOS1 PROD1 WHERE CODIGO=2;
+dbms_output.put_line(c1);
+
+--soporta info de tipo no escalar
+SELECT JSON_query(PROD1.DATOS,'$.direccion') INTO C1 FROM PRODUCTOS1 PROD1 WHERE CODIGO=2;
+dbms_output.put_line(c1);
+
+-- se pone la columna concreta que querias (datos)
+SELECT JSON_transform(datos,rename '$.poblacion'='pob') INTO C1 FROM PRODUCTOS1 PROD1 WHERE CODIGO=2;
+dbms_output.put_line(c1);
+
+end;
+/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
